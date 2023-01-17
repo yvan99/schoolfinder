29a0425850a0015e7753e272d760bf61b63264a9
yvan99 <ishimweyvan90@gmail.com>
2023-01-17 16:29:51 +0200

    add google restrictor

--- includes/script.php
+++ includes/script.php
@@ -1,10 +1,17 @@
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places&key=AIzaSyDpuMMt3CR3XvDhPy1do7Q1KMY0q2G0WOc"></script>
 <script>
-    function init() {
+    const googlePlacesInit=()=> {
+        // restrict googlePlaces to rwanda places
+        const options = {
+            componentRestrictions: {
+                country: "rw"
+            }
+        };
+
         let input = document.getElementById('locationTextField');
         let autocomplete = new google.maps.places.Autocomplete(input);
     }
-    google.maps.event.addDomListener(window, 'load', init);
+    google.maps.event.addDomListener(window, 'load', googlePlacesInit);
 </script>
 </body>
 
