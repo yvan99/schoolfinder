fcc815e503bbfcadf053c273fa8ca7d35d19826c
yvan99 <ishimweyvan90@gmail.com>
2023-01-25 23:58:19 +0200

    added changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,10 @@
 <footer class="container-fluid p-3 bg-dark text-white">
-      <div class="d-lg-flex justify-content-between">
+    <div class="d-lg-flex justify-content-between">
         <div>
-          <span>SCHOOL FINDER SYSTEM</span>
+            <span>SCHOOL FINDER SYSTEM</span>
         </div>
         <div class="copyright">
-          <p>Developed and maintained by <a href="#" target="_blank">Your names</a></p>
+            <p>Developed and maintained by <a href="#" target="_blank">Your names</a></p>
         </div>
-      </div>
-    </footer>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -4,13 +4,6 @@ require_once ('includes/css.php');
 require_once ('includes/navbar.php');
 require_once ('includes/hero.php');
 require_once ('includes/footer.php');
-?>
-
-
-
-
-<?php 
-// call css file
 require_once ('includes/script.php');
 ?>
     
