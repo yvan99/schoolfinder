e3cf678c9d3cf724ffdcf8c2337df051e6a93216
yvan99 <ishimweyvan90@gmail.com>
2023-01-25 23:54:03 +0200

    added names

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,10 @@
 <footer class="container p-4 rounded">
       <div class="d-lg-flex justify-content-between">
         <div>
-          <span>LOGO</span>
+          <span>SCHOOL FINDER SYSTEM</span>
         </div>
         <div class="copyright">
-          <p>developed and maintained by <a href="#" target="_blank">company</a></p>
+          <p>developed and maintained by <a href="#" target="_blank">Your names</a></p>
         </div>
       </div>
     </footer>
