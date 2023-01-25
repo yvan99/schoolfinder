a8ab74eb7ce59f3a225f3df7279960d79ada60cb
yvan99 <ishimweyvan90@gmail.com>
2023-01-25 23:52:01 +0200

    add footer

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<footer class="container p-4 rounded">
+      <div class="d-lg-flex justify-content-between">
+        <div>
+          <span>LOGO</span>
+        </div>
+        <div class="copyright">
+          <p>developed and maintained by <a href="#" target="_blank">company</a></p>
+        </div>
+        <div>
+          <ul class="d-flex gap-3 list-unstyled">
+            <li><a href="#"><i class="bi bi-facebook"></i></a></li>
+            <li><a href="#"><i class="bi bi-twitter"></i></a></li>
+            <li><a href="#"><i class="bi bi-instagram"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
