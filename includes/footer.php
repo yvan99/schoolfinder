a17d57ca6fb0be44a75b6d8ad68d7d59b9422615
yvan99 <ishimweyvan90@gmail.com>
2023-01-25 23:55:17 +0200

    add fluid class

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,10 @@
-<footer class="container p-4 rounded">
+<footer class="container-fluid p-4 rounded">
       <div class="d-lg-flex justify-content-between">
         <div>
           <span>SCHOOL FINDER SYSTEM</span>
         </div>
         <div class="copyright">
-          <p>developed and maintained by <a href="#" target="_blank">Your names</a></p>
+          <p>Developed and maintained by <a href="#" target="_blank">Your names</a></p>
         </div>
       </div>
     </footer>
