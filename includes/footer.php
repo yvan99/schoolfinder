822d4a2873c55d36e406492741082d6142e9ad8f
yvan99 <ishimweyvan90@gmail.com>
2023-01-25 23:53:36 +0200

    remove icons from footer

--- includes/footer.php
+++ includes/footer.php
@@ -6,12 +6,5 @@
         <div class="copyright">
           <p>developed and maintained by <a href="#" target="_blank">company</a></p>
         </div>
-        <div>
-          <ul class="d-flex gap-3 list-unstyled">
-            <li><a href="#"><i class="bi bi-facebook"></i></a></li>
-            <li><a href="#"><i class="bi bi-twitter"></i></a></li>
-            <li><a href="#"><i class="bi bi-instagram"></i></a></li>
-          </ul>
-        </div>
       </div>
     </footer>
